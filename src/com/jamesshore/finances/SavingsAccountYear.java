6632c02ba50eaa68a24736b17f84f3c925502302
James Shore <jshore@jamesshore.com>
2010-09-01 13:47:04 -0700

    End of episode 4

---
+++ src/com/jamesshore/finances/SavingsAccountYear.java
@@ -0,0 +1,42 @@
+package com.jamesshore.finances;
+
+public class SavingsAccountYear {
+
+	private int startingBalance = 0;
+	private int capitalGainsAmount = 0;
+	private int interestRate = 0;
+	
+	public SavingsAccountYear() {}
+	
+	public SavingsAccountYear(int startingBalance, int interestRate) {
+		this.startingBalance = startingBalance;
+		this.interestRate = interestRate;
+	}
+	
+	public SavingsAccountYear(int startingBalance, int capitalGainsAmount, int interestRate) {
+		this.startingBalance = startingBalance;
+		this.capitalGainsAmount = capitalGainsAmount;
+		this.interestRate = interestRate;
+	}
+
+	public int startingBalance() {
+		return startingBalance;
+	}
+
+	public int interestRate() {
+		return interestRate;
+	}
+
+	public int endingBalance() {
+		return startingBalance + (startingBalance * interestRate / 100);
+	}
+
+	public SavingsAccountYear nextYear() {
+		return new SavingsAccountYear(this.endingBalance(), interestRate);
+	}
+
+	public void withdraw(int amount) {
+		startingBalance -= amount;
+	}
+
+}
---
+++ src/com/jamesshore/finances/_SavingsAccountYearTest.java
@@ -0,0 +1,57 @@
+package com.jamesshore.finances;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class _SavingsAccountYearTest {
+
+	@Test
+	public void startingBalanceMatchesConstructor() {
+		assertEquals(10000, newAccount().startingBalance());
+	}
+	
+	@Test
+	public void interestRateMatchesConstructor() {
+		assertEquals(10, newAccount().interestRate());
+	}
+	
+	@Test
+	public void endingBalanceAppliesInterestRate() {
+		assertEquals(11000, newAccount().endingBalance());
+	}
+	
+	@Test
+	public void nextYearsStartingBalanceEqualsThisYearsEndingBalance() {
+		SavingsAccountYear thisYear = newAccount();
+		assertEquals(thisYear.endingBalance(), thisYear.nextYear().startingBalance());
+	}
+	
+	@Test
+	public void nextYearsInterestRateEqualsThisYearsInterestRate() {
+		SavingsAccountYear thisYear = newAccount();
+		assertEquals(thisYear.interestRate(), thisYear.nextYear().interestRate());
+	}
+	
+	@Test
+	public void withdrawingFundsOccursAtTheBeginningOfTheYear() {
+		SavingsAccountYear year = new SavingsAccountYear(10000, 10);
+		year.withdraw(1000);
+		assertEquals(9900, year.endingBalance());
+	}
+	
+	@Test
+	public void withdrawingMoreThanPrincipalIncursCapitalGainsTax() {
+		SavingsAccountYear year = new SavingsAccountYear(10000, 7000, 10);
+		year.withdraw(3000);
+		assertEquals(7700, year.endingBalance());
+		year.withdraw(5000);
+		assertEquals(2000 + 200 - (1250), year.endingBalance());
+	}
+
+	private SavingsAccountYear newAccount() {
+		SavingsAccountYear account = new SavingsAccountYear(10000, 10);
+		return account;
+	}
+	
+}
