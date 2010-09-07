eb4733e34f54364db4207adfb58f6e0b7576ca4d
James Shore <jshore@jamesshore.com>
2010-09-07 12:23:55 -0700

    End of episode 14.

--- src/com/jamesshore/finances/TaxRate.java
+++ src/com/jamesshore/finances/TaxRate.java
@@ -1,17 +1,48 @@
 package com.jamesshore.finances;
 
-import java.math.*;
-
 public class TaxRate {
 
-	private BigDecimal rate;
+	private double rate;
 	
 	public TaxRate(double rateAsPercentage) {
-		this.rate = new BigDecimal(rateAsPercentage).divide(new BigDecimal(100));
+		this.rate = rateAsPercentage / 100.0;
 	}
 
-	public int taxFor(int amount) {
-		return (int)(amount. * rate);
+	public int rate() {
+		return (int)(rate * 100);  // TODO: delete me
+	}
+	
+	public int simpleTaxFor(int amount) {
+		return (int)(rate * amount);
+	}
+
+	public int compoundTaxFor(int amount) {
+		return (int)((amount / (1 - rate)) - amount);
+	}
+	
+	@Override
+	public String toString() {
+		return (rate * 100) + "%";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(rate);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		TaxRate other = (TaxRate) obj;
+		if (Double.doubleToLongBits(rate) != Double.doubleToLongBits(other.rate)) return false;
+		return true;
 	}
 
 }
--- src/com/jamesshore/finances/_TaxRateTest.java
+++ src/com/jamesshore/finances/_TaxRateTest.java
@@ -9,13 +9,31 @@ public class _TaxRateTest {
 	@Test
 	public void nothing() {
 		TaxRate taxRate = new TaxRate(0);
-		assertEquals(0, taxRate.taxFor(1000));
+		assertEquals(0, taxRate.simpleTaxFor(1000));
+		assertEquals(0, taxRate.compoundTaxFor(1000));
 	}
 	
 	@Test
-	public void simpleTax() {
+	public void simpleTaxJustAppliesTaxRateToAmount() {
 		TaxRate taxRate = new TaxRate(25);
-		assertEquals(250, taxRate.taxFor(1000));
+		assertEquals(250, taxRate.simpleTaxFor(1000));
 	}
 	
+	@Test
+	public void compoundTaxIsTheAmountOfTaxThatIsIncurredIfYouAlsoPayTaxOnTheTax() {
+		TaxRate taxRate = new TaxRate(25);
+		assertEquals(333, taxRate.compoundTaxFor(1000));
+	}
+	
+	@Test
+	public void valueObject() {
+		TaxRate rate1a = new TaxRate(33);
+		TaxRate rate1b = new TaxRate(33);
+		TaxRate rate2 = new TaxRate(40);
+		
+		assertEquals("33.0%", rate1a.toString());
+		assertTrue("same values should be equal", rate1a.equals(rate1b));
+		assertFalse("different values should not be equal", rate1a.equals(rate2));
+		assertTrue("same values have same hash code", rate1a.hashCode() == rate1b.hashCode());
+	}
 }
