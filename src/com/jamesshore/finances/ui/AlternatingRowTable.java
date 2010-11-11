d16955f2fbae373882d426080c9d7fd6272fb1a1
James Shore <jshore@jamesshore.com>
2010-11-11 15:13:03 +0100

    End of episode 49

---
+++ src/com/jamesshore/finances/ui/AlternatingRowTable.java
@@ -0,0 +1,15 @@
+package com.jamesshore.finances.ui;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+public class AlternatingRowTable extends JTable {
+
+	public static final Color DEFAULT_BACKGROUND_COLOR = Color.WHITE;
+
+	public AlternatingRowTable(TableModel model) {
+		super(model);
+	}
+
+}
--- src/com/jamesshore/finances/ui/_AlternatingRowTableTest.java
+++ src/com/jamesshore/finances/ui/_AlternatingRowTableTest.java
@@ -1,15 +1,31 @@
 package com.jamesshore.finances.ui;
 
 import static org.junit.Assert.*;
+import java.awt.*;
+import javax.swing.table.*;
 import org.junit.*;
 
 
 public class _AlternatingRowTableTest {
 
 	@Test
-	public void shouldExistWithZeroRowsWhenCreated() {
-		AlternatingRowTable table = new AlternatingRowTable();
+	public void tableWithJustOneCell_CellUsesDefaultBackgroundColor() {
+		Object[][] testData = new Object[][] { new Object[] { "" } };
+		Object[] noColumnNames = null;
+		TableModel model = new DefaultTableModel(testData, noColumnNames);
 		
+		AlternatingRowTable table = new AlternatingRowTable(model);
+		
+		TableCellRenderer renderer = table.getCellRenderer(0, 0);
+		Component component = table.prepareRenderer(renderer, 0, 0);
+		Color actualColor = component.getBackground();
+		
+		assertEquals(AlternatingRowTable.DEFAULT_BACKGROUND_COLOR, actualColor);
 	}
+	
+	//@Test
+	//public void secondRowShouldHaveAlternateBackgroundColor() {
+	
+	
 	
 }
