9bd7456031fdec7377baca75d9d3bf5804309e67
James Shore <jshore@jamesshore.com>
2011-05-27 13:11:48 -0700

    End of episode 115

--- src/com/jamesshore/spikes/icon_in_textfield/IconInTextField.java
+++ src/com/jamesshore/spikes/icon_in_textfield/IconInTextField.java
@@ -1,11 +1,14 @@
 package com.jamesshore.spikes.icon_in_textfield;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.net.*;
 import javax.swing.*;
 
 public class IconInTextField extends JFrame {
 	private static final long serialVersionUID = 1L;
+
+	private JLabel iconLabel;
 
 	public IconInTextField() {
 		setTitle("Icon in Textfield Spike");
@@ -17,13 +20,14 @@ public class IconInTextField extends JFrame {
 
 	private void addComponents() {
 		Container content = this.getContentPane();
-		content.add("Center", iconTextFieldWithGridBag());
-		// content.add("Center", iconTextFieldWithOverlay());
+		// content.add("Center", iconTextFieldWithGridBag());
+		content.add("Center", iconTextFieldWithOverlayLayout());
+		content.add("South", button());
 		// content.add("Center", textField());
 		// content.add("East", icon());
 	}
 
-	private Component iconTextFieldWithOverlay() {
+	private Component iconTextFieldWithOverlayLayout() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new OverlayLayout(panel));
 
@@ -31,16 +35,22 @@ public class IconInTextField extends JFrame {
 		iconPanel.setOpaque(false);
 		iconPanel.add(icon());
 
-		JPanel gridbag = new JPanel();
-		gridbag.setLayout(new GridBagLayout());
-		gridbag.add(textField(), new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+		JPanel textPanel = new JPanel(new BorderLayout());
+		textPanel.add(textField(), BorderLayout.NORTH);
+		textPanel.setOpaque(false);
+
+		// JPanel textPanel = new JPanel();
+		// textPanel.setLayout(new GridBagLayout());
+		// textPanel.add(textField(), new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTH,
+		// GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
 
 		panel.add(iconPanel);
-		panel.add(gridbag);
+		panel.add(textPanel);
 		return panel;
 	}
 
-	private Component iconTextFieldWithGridBag() {
+	@SuppressWarnings("unused")
+	private Component iconTextFieldWithSideBySideLayout() {
 		JPanel gridbag = new JPanel();
 		gridbag.setLayout(new GridBagLayout());
 
@@ -66,13 +76,24 @@ public class IconInTextField extends JFrame {
 	}
 
 	private JLabel icon() {
-		JLabel iconLabel = new JLabel();
+		iconLabel = new JLabel();
 		URL iconUrl = getClass().getResource("invalid_dollars.png");
 		ImageIcon icon = new ImageIcon(iconUrl, "Invalid dollar amount");
 		iconLabel.setIcon(icon);
 		iconLabel.setBackground(Color.WHITE);
 		iconLabel.setOpaque(false);
+
 		return iconLabel;
+	}
+
+	private Component button() {
+		final JButton button = new JButton("Toggle Warning");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				iconLabel.setVisible(!iconLabel.isVisible());
+			}
+		});
+		return button;
 	}
 
 	public static void main(String[] args) {
